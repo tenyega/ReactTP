cdff10457d3e3d00255d6eb3faf6f6e09a31a468
yega <mdolma@ymail.com>
2024-07-05 17:00:59 +0200

    submissions

--- src/partials/fact.tsx
+++ src/partials/fact.tsx
@@ -13,10 +13,8 @@ export default function Facts () {
 
           <div className="col-lg-3 col-md-6">
             <div className="count-box">
-            <img src="https://api.iconify.design/ph:smiley.svg?color=%23888888" alt="Home" className="h-6 w-6"/>
-
-              <i className="bi bi-emoji-smile"></i>
-              <span data-purecounter-start="0" data-purecounter-end="232" data-purecounter-duration="1" className="purecounter"></span>
+            <i className="bi bi-emoji-smile"></i>
+              <span data-purecounter-start="0" data-purecounter-end="232" data-purecounter-duration="1" className="purecounter">232</span>
               <p>Happy Clients</p>
             </div>
           </div>
@@ -24,7 +22,7 @@ export default function Facts () {
           <div className="col-lg-3 col-md-6 mt-5 mt-md-0">
             <div className="count-box">
               <i className="bi bi-journal-richtext"></i>
-              <span data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="1" className="purecounter"></span>
+              <span data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="1" className="purecounter">521</span>    
               <p>Projects</p>
             </div>
           </div>
@@ -32,7 +30,7 @@ export default function Facts () {
           <div className="col-lg-3 col-md-6 mt-5 mt-lg-0">
             <div className="count-box">
               <i className="bi bi-headset"></i>
-              <span data-purecounter-start="0" data-purecounter-end="1463" data-purecounter-duration="1" className="purecounter"></span>
+              <span data-purecounter-start="0" data-purecounter-end="1463" data-purecounter-duration="1" className="purecounter">1463</span>
               <p>Hours Of Support</p>
             </div>
           </div>
@@ -40,7 +38,7 @@ export default function Facts () {
           <div className="col-lg-3 col-md-6 mt-5 mt-lg-0">
             <div className="count-box">
               <i className="bi bi-award"></i>
-              <span data-purecounter-start="0" data-purecounter-end="25" data-purecounter-duration="1" className="purecounter"></span>
+              <span data-purecounter-start="0" data-purecounter-end="25" data-purecounter-duration="1" className="purecounter">25</span>
               <p>Awards</p>
             </div>
           </div>
